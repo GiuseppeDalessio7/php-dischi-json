aaf19860c96c9e5fddb2ab336bc0b03b34534c1d
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-10-12 16:53:20 +0200

    Fix layout index

--- index.php
+++ index.php
@@ -15,18 +15,38 @@
 
 <body>
 
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">La mia libreria di dischi</h1>
 
+            <div class="dark modal fade" :id="`${album.author.replace(/\s+/g, '')}${index}`" tabindex="-1" :aria-labelledby="`${album.author.replace(/\s+/g, '')}${index}Label`" aria-hidden="true" v-for="(album, index) in list" :key="`${album.author.replace(/\s+/g, '')}${index}`">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="album card border-0 h-100 modal-body">
+                            <img class="img-card-top py-4 px-5" :src="album.poster" alt="">
+                            <div class="card-body text-center text-white">
+                                <h2>{{album.title}}</h2>
+                                <p>{{album.author}}</p>
+                                <h4>{{album.year}}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-    <h1 class="text-center">La mia libreria dei dischi</h1>
-
-    <div v-for="album in disc" class="card" style="background-color:orangered; border-color:darkblue;">
-        <img class="card-img-top" src="disc.poster" alt="Title">
-        <div class="card-body">
-            <h4 class="card-title">{{disc.title}}</h4>
-            <p class="card-text">{{disc.year}}</p>
+            <div class="col-4 g-5" v-for="(album, index) in list" :key="`${album.author.replace(/\s+/g, '')}${index}`">
+                <div class="album card border-0 h-100" data-bs-toggle="modal" :data-bs-target="`#${album.author.replace(/\s+/g, '')}${index}`" :id="`${album.author.replace(/\s+/g, '')}${index}`">
+                    <img class="img-card-top py-4 px-5" :src="album.poster" alt="">
+                    <div class="card-body text-center text-white">
+                        <h3>{{album.title}}</h3>
+                        <p>{{album.author}}</p>
+                        <h5>{{album.year}}</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
+    </main>
 
 
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
