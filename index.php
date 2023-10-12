fb9a53cf9309dfce7d7ea999fa5ff4d5d32fe200
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-10-12 17:28:26 +0200

    Finish milestone, try to bonus

--- index.php
+++ index.php
@@ -15,38 +15,41 @@
 
 <body>
 
-    <div class="container">
-        <div class="row">
-            <h1 class="text-center">La mia libreria di dischi</h1>
 
-            <div class="dark modal fade" :id="`${album.author.replace(/\s+/g, '')}${index}`" tabindex="-1" :aria-labelledby="`${album.author.replace(/\s+/g, '')}${index}Label`" aria-hidden="true" v-for="(album, index) in list" :key="`${album.author.replace(/\s+/g, '')}${index}`">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="album card border-0 h-100 modal-body">
-                            <img class="img-card-top py-4 px-5" :src="album.poster" alt="">
-                            <div class="card-body text-center text-white">
-                                <h2>{{album.title}}</h2>
-                                <p>{{album.author}}</p>
-                                <h4>{{album.year}}</h4>
+    <div id="app">
+        <div class="container">
+            <div class="row" style="background-color: #333;">
+                <h1 class="text-center red_c">La mia libreria di dischi</h1>
+
+                <div class="dark modal fade " :id="`${album.author.replace(/\s+/g, '')}${index}`" tabindex="-1" :aria-labelledby="`${album.author.replace(/\s+/g, '')}${index}Label`" aria-hidden="true" v-for="(album, index) in mylist" :key="`${album.author.replace(/\s+/g, '')}${index}`">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="album card border-0 h-100 modal-body">
+                                <img class="img-card-top" style="background-color: greenyellow;" :src="album.poster" alt="">
+                                <div class="card-body text-center">
+                                    <h2>{{album.title}}</h2>
+                                    <p>{{album.author}}</p>
+                                    <h4>{{album.year}}</h4>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-4 g-5" v-for="(album, index) in list" :key="`${album.author.replace(/\s+/g, '')}${index}`">
-                <div class="album card border-0 h-100" data-bs-toggle="modal" :data-bs-target="`#${album.author.replace(/\s+/g, '')}${index}`" :id="`${album.author.replace(/\s+/g, '')}${index}`">
-                    <img class="img-card-top py-4 px-5" :src="album.poster" alt="">
-                    <div class="card-body text-center text-white">
-                        <h3>{{album.title}}</h3>
-                        <p>{{album.author}}</p>
-                        <h5>{{album.year}}</h5>
+                <div class="col-4 g-5" v-for="(album, index) in mylist" :key="`${album.author.replace(/\s+/g, '')}${index}`">
+                    <div class="album card border-0 h-100" data-bs-toggle="modal" :data-bs-target="`#${album.author.replace(/\s+/g, '')}${index}`" :id="`${album.author.replace(/\s+/g, '')}${index}`">
+                        <img class="img-card-top py-3 px-4 img_card" :src="album.poster" alt="">
+                        <div class="card-body text-center text-white img_card">
+                            <h2>{{album.title}}</h2>
+                            <p>{{album.author}}</p>
+                            <h4>{{album.year}}</h4>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </main>
+
 
 
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
